103d7d8dec2d3a0ca3d80e4f86a66b635e014fac
salazarna <59241301+salazarna@users.noreply.github.com>
2023-10-15 19:16:30 -0500

    Update version.py

--- src/version.py
+++ src/version.py
@@ -10,5 +10,6 @@ CHANGE LOG
 0.1.0 : Initial release of the library @ March, 2023.
 0.2.0 : Adjustments following the first request for reviews @ May 31, 2023.
 0.3.0 : Adjustments following the second request for reviews @ August 18, 2023.
+1.0.0 : Official release of the published article @ September 21, 2023.
 '''
 __version__ = '0.3.0'
